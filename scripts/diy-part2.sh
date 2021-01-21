a6d74a4e9f5a6030b5139ed6c6b45fc6c1285c04
aicccu <72127637+aicccu@users.noreply.github.com>
2021-01-21 17:42:03 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -1,9 +1,11 @@
 # Diy-Part2
 
+# Add lean's package
 pushd package/lean
 git clone --depth=1 https://github.com/fw876/helloworld
 popd
 
+# Add kenzok8's package
 pushd package
 git clone --depth=1 https://github.com/kenzok8/openwrt-packages
 git clone --depth=1 https://github.com/kenzok8/small

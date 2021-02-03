2e9aafd6b9f9d1da2f5b6bff359a249feccfce9e
aicccu <wesyx@outlook.com>
2021-02-03 13:52:09 +0800

    Update

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -5,11 +5,10 @@ git clone --depth=1 https://github.com/fw876/helloworld
 popd
 
 mkdir package/community
-pushd package/community
 
+pushd package/community
 git clone --depth=1 https://github.com/kenzok8/openwrt-packages
 git clone --depth=1 https://github.com/kenzok8/small
-
 popd
 
 sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd

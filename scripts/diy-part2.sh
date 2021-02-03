9307e3e7da30082810d114f1f166f29e9dc9d483
aicccu <wesyx@outlook.com>
2021-02-03 10:15:58 +0800

    update

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -1,16 +1,15 @@
 # Diy-Part2
 
-# Add others package
+pushd package/lean
+git clone --depth=1 https://github.com/fw876/helloworld
+popd
+
 mkdir package/community
 pushd package/community
 
-git clone --depth=1 -b master https://github.com/vernesong/OpenClash
-
-git clone --depth=1 https://github.com/aicccu/luci-theme-opentomcat
-
-svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome
+git clone --depth=1 https://github.com/kenzok8/openwrt-packages
+git clone --depth=1 https://github.com/kenzok8/small
 
 popd
 
-# Change default shell to zsh
 sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd

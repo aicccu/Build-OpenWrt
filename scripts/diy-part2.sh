d0b5601233ba7c4b3ecb4c7c9419cc5d08e29a0c
aicccu <72127637+aicccu@users.noreply.github.com>
2021-03-23 20:22:16 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -11,6 +11,9 @@ pushd package/community
 # Add luci-app-adguardhome
 git clone --depth=1 https://github.com/SuLingGG/luci-app-adguardhome.git
 
+# Add luci-app-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
+
 # Add OpenClash
 git clone --depth=1 -b master https://github.com/vernesong/OpenClash.git
 
@@ -18,10 +21,6 @@ git clone --depth=1 -b master https://github.com/vernesong/OpenClash.git
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git
 rm -rf ../lean/luci-theme-argon
-
-# Add driver for rtl8821cu & rtl8812au-ac
-svn co https://github.com/project-openwrt/openwrt/branches/master/package/ctcgfw/rtl8812au-ac
-svn co https://github.com/project-openwrt/openwrt/branches/master/package/ctcgfw/rtl8821cu
 
 popd
 
